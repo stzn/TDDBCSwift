83f8df959da76e14b3b7001c5017202a18b9e296
shiz <sztk1209@gmail.com>
2018-10-01 06:23:27 +0900

    リモートとの通信クラスのTODO、テスト、URLSessionのモックを作成。 RemoteStockFetcherを作成してレッドになることを確認。

---
+++ TDDBCSwift/RemoteStockFetcher.swift
@@ -0,0 +1,39 @@
+//
+//  RemoteStockFetcher.swift
+//  TDDBCSwift
+//
+//  Created by Shinzan Takata on 2018/10/01.
+//  Copyright © 2018 tddbc. All rights reserved.
+//
+
+import Foundation
+
+struct RemoteStockFetcher: RemoteStockFechable {
+    
+    let urlSession: SessionProtocol
+    
+    func getStocks(of beverage: Beverage, completion: (Data?, URLResponse?, Error?) -> Void) {
+        completion(nil, nil, nil)
+    }
+}
+
+protocol SessionProtocol {
+    func dataTask(with url: URL,
+                  completionHandler: @escaping ResponseHandler) -> URLSessionDataTask
+    
+    func dataTask(with request: URLRequest,
+                  completionHandler: @escaping ResponseHandler) -> URLSessionDataTask
+}
+
+extension URLSession: SessionProtocol {}
+
+extension String {
+    var percentEncoded: String {
+        let allowedCharacters = CharacterSet( charactersIn: "/%&=?$#+-~@<>|\\*,.()[]{}^!").inverted
+        
+        guard let encoded = self.addingPercentEncoding(withAllowedCharacters: allowedCharacters) else {
+            fatalError()
+        }
+        return encoded
+    }
+}
---
+++ TDDBCSwiftTests/XCTest/RemoteStockFetcherTests.swift
@@ -0,0 +1,93 @@
+//
+//  RemoteStockFetcherTests.swift
+//  TDDBCSwiftTests
+//
+//  Created by Shinzan Takata on 2018/10/01.
+//  Copyright © 2018 tddbc. All rights reserved.
+//
+
+import XCTest
+@testable import TDDBCSwift
+
+class RemoteStockFetcherTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    // TODO
+    // リモートと正しく通信ができる
+    //   コーラの在庫数を正しいURLで通信した場合、正常なレスポンスを返す
+    //   コーヒーの在庫数を正しいURLで通信した場合、正常なレスポンスを返す
+    //   コーラの在庫数を間違ったURLで通信したがサーバーからエラーが返ってきた場合、エラーレスポンスを返す
+    func test_コーラの在庫数を正しいURLで通信した場合_正常なレスポンスを返す() {
+        let urlSession = MockURLSession(data: nil, urlResponse: nil, error: nil)
+        
+        let fetcher = RemoteStockFetcher(urlSession: urlSession)
+        fetcher.getStocks(of: .cola) { data, response, error in
+            let expected = URL(string: "https://vending.com/stock?type=cola")
+            XCTAssertEqual(urlSession.url, expected)
+        }
+    }
+}
+
+extension RemoteStockFetcherTests {
+    
+    class MockURLSession: SessionProtocol {
+        
+        var url: URL?
+        var request: URLRequest?
+        let dataTask: MockTask
+        
+        var urlComponents: URLComponents? {
+            guard let url = url else {
+                return nil
+            }
+            return URLComponents(url: url, resolvingAgainstBaseURL: true)
+        }
+        
+        init(data: Data?, urlResponse: URLResponse?, error: Error?) {
+            dataTask = MockTask(data: data, urlResponse: urlResponse, error: error)
+        }
+        
+        func dataTask(with url: URL, completionHandler: @escaping ResponseHandler) -> URLSessionDataTask {
+            self.url = url
+            dataTask.responseHandler = completionHandler
+            return dataTask
+        }
+        
+        func dataTask(with request: URLRequest, completionHandler: @escaping ResponseHandler) -> URLSessionDataTask {
+            self.request = request
+            dataTask.responseHandler = completionHandler
+            return dataTask
+        }
+    }
+    
+    class MockTask: URLSessionDataTask {
+        
+        private let data: Data?
+        private let urlResponse: URLResponse?
+        private let responseError: Error?
+        
+        var responseHandler: ResponseHandler?
+        
+        init(data: Data?, urlResponse: URLResponse?, error: Error?) {
+            self.data = data
+            self.urlResponse = urlResponse
+            self.responseError = error
+        }
+        
+        override func resume() {
+            DispatchQueue.main.async {
+                self.responseHandler?(self.data,
+                                      self.urlResponse,
+                                      self.responseError)
+            }
+        }
+    }
+    
+}
