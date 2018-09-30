7dddc28eb3d51c010d3c7d03630dc99c6c0a7c78
shiz <sztk1209@gmail.com>
2018-09-30 18:29:29 +0900

    RemoteManagerTestsクラスを追加

---
+++ TDDBCSwiftTests/XCTest/RemoteManagerTests.swift
@@ -0,0 +1,21 @@
+//
+//  RemoteManagerTests.swift
+//  TDDBCSwiftTests
+//
+//  Created by Shinzan Takata on 2018/09/30.
+//  Copyright © 2018 tddbc. All rights reserved.
+//
+
+import XCTest
+@testable import TDDBCSwift
+
+class RemoteManagerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
