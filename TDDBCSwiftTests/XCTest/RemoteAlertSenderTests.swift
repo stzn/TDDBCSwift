ff8e038b61c43876612d4e1ead944500fc8044de
shiz <sztk1209@gmail.com>
2018-10-01 16:22:20 +0900

    「コーヒーの在庫数が少ないというアラートを正しいRequestでPOST通信した場合_正常なレスポンスを返す」ケースを追加し、グリーンになることを確認

--- TDDBCSwiftTests/XCTest/RemoteAlertSenderTests.swift
+++ TDDBCSwiftTests/XCTest/RemoteAlertSenderTests.swift
@@ -21,12 +21,40 @@ class RemoteAlertSenderTests: XCTestCase {
     
     // TODO
     // リモートと正しく通信してアラートを通知する
-    //  コーラの在庫数が少ないというアラートを正しいRequestでPOST通信した場合、正常なレスポンスを返す
+    //  xコーラの在庫数が少ないというアラートを正しいRequestでPOST通信した場合、正常なレスポンスを返す
     //  コーヒーの在庫数が少ないというアラートを正しいRequestでPOST通信した場合、正常なレスポンスを返す
     //  サーバーからエラーが返ってきた場合、エラーレスポンスを返す
     //  サーバーからレスポンスのステータスコードが399の場合、正常なレスポンスを返す
     //  サーバーからレスポンスのステータスコードが199の場合、エラーレスポンスを返す
     //  サーバーからレスポンスのステータスコードが400の場合、エラーレスポンスを返す
+
+    func test_コーヒーの在庫数が少ないというアラートを正しいRequestでPOST通信した場合_正常なレスポンスを返す() {
+
+        let url = URL(string: "https://vending.com/alert")!
+        let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
+        let urlSession = MockURLSession(data: Data(), urlResponse: response, error: nil)
+        let sender = RemoteAlertSender(urlSession: urlSession)
+        var success: Bool = false
+        
+        let exp = expectation(description: "コーヒーの在庫数が少ないというアラートを正しいRequestでPOST通信した場合、正常なレスポンスを返す")
+        sender.sendAlert(of: .coffee) { result in
+            success = result
+            exp.fulfill()
+        }
+        waitForExpectations(timeout: 3) { error in
+            
+            let data = urlSession.request?.httpBody ?? Data()
+            guard let parameter = try? JSONDecoder().decode([String: String].self, from: data),
+                let name = parameter["name"],
+                let beverage = Beverage(rawValue: name) else {
+                XCTFail()
+                return
+            }
+            XCTAssertEqual(beverage, .coffee)
+            XCTAssertEqual(urlSession.request?.httpMethod, "POST")
+            XCTAssertTrue(success)
+        }
+    }
     
     func test_コーラの在庫数が少ないというアラートを正しいRequestで通信した場合_正常なレスポンスを返す() {
         
@@ -42,7 +70,15 @@ class RemoteAlertSenderTests: XCTestCase {
             exp.fulfill()
         }
         waitForExpectations(timeout: 3) { error in
-            XCTAssertNotNil(urlSession.request?.httpBody)
+
+            let data = urlSession.request?.httpBody ?? Data()
+            guard let parameter = try? JSONDecoder().decode([String: String].self, from: data),
+                let name = parameter["name"],
+                let beverage = Beverage(rawValue: name) else {
+                    XCTFail()
+                    return
+            }
+            XCTAssertEqual(beverage, .cola)
             XCTAssertEqual(urlSession.request?.httpMethod, "POST")
             XCTAssertTrue(success)
         }
